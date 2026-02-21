d71be154930ff36e325a0cf92a493c28a838cf54
Padraic O Loughlin <g00468865@atu.ie>
2026-02-21 12:33:06 +0000

    Initial Code

---
+++ src/main/java/ie/atu/oopsem2lab4/OopSem2Lab4Application.java
@@ -0,0 +1,13 @@
+package ie.atu.oopsem2lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopSem2Lab4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopSem2Lab4Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/oopsem2lab4/OopSem2Lab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.oopsem2lab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopSem2Lab4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
